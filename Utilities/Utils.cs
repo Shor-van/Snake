23c776ed3f63b0093060a121da0610eefcbc36c9
Shor-van <shorxvan01@gmail.com>
2023-10-30 17:14:51 +0100

    Added IsWithinWindowBoundery(int x, int y, int width, int height)

--- Utilities/Utils.cs
+++ Utilities/Utils.cs
@@ -10,5 +10,13 @@ namespace Snake.Utilities
         /// <param name="y">The Y location to check</param>
         /// <returns>True if the location is within the boundery, false if not</returns>
         internal static bool IsWithinWindowBoundery(int x, int y) => x >= 0 && x < Console.BufferWidth && y >= 0 && y < Console.BufferHeight;
+
+        /// <summary>Checks if the given 'rectangle' is within the <see cref="Console"/> window boundery</summary>
+        /// <param name="x">The left most point of the rectangle</param>
+        /// <param name="x">The top most point of the rectangle</param>
+        /// <param name="width">The width of the rectangle</param>
+        /// <param name="height">The height of the rectangle</param>
+        /// <returns>True if the 'rectangle' is within the boundery, false if not</returns>
+        internal static bool IsWithinWindowBoundery(int x, int y, int width, int height) => x >= 0 && x < Console.BufferWidth && y >= 0 && y < Console.BufferHeight && x + width < Console.BufferWidth && y + height < Console.BufferHeight;
     }
 }
