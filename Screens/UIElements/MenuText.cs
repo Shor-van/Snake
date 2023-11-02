bfc964d87940503d2d46abd8ce96ef1da212fe65
Shor-van <shorxvan01@gmail.com>
2023-11-02 01:05:44 +0100

    Frame for MenuText

--- Screens/UIElements/MenuText.cs
+++ Screens/UIElements/MenuText.cs
@@ -1,7 +1,39 @@
+using System;
+
+using Snake.Utilities;
+
 namespace Snake.Screens
 {
     /// <summary></summary>
     internal sealed class MenuText
     {
+        private int x, y; //the X and Y position of the menu text
+        private string text; //the text of the menu text
+        private bool highlighted; //wether the menu text is highlighted(selceted)
+        private ConsoleColor color, highlightedColor; //the colors used to draw the text
+
+        /// <summary></summary>
+        internal event EventHandler Selected;
+
+        /// <summary></summary>
+        internal event EventHandler OnHighlighted;
+
+        /// <summary></summary>
+        internal event EventHandler OnUnhighlighted;
+
+        internal void HandleInput(GameTime gameTime)
+        {
+            
+        }
+
+        internal void Update(GameTime gameTime)
+        {
+
+        }
+
+        internal void Draw(GameTime gameTime)
+        {
+
+        }
     }
 }
