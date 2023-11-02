1791816e54ef5588992ae91bd814bb3111403798
Shor-van <shorxvan01@gmail.com>
2023-11-02 01:07:21 +0100

    Added TextLabel(WIP)

--- Screens/UIElements/TextLabel.cs
+++ Screens/UIElements/TextLabel.cs
@@ -1,7 +1,12 @@
+using System;
+
 namespace Snake.Screens
 {
     /// <summary></summary>
     internal sealed class TextLabel
     {
+        private int x, y; //the X and Y position of the text label
+        private string text; //the text of the text label
+        private ConsoleColor color; //the color used to draw the text
     }
 }
