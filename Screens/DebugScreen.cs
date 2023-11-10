e9d3473f55ec21dcdd4a95ff366a2216e36026b2
Shor-van <shorxvan01@gmail.com>
2023-11-10 12:08:42 +0100

    Added Format method

--- Screens/DebugScreen.cs
+++ Screens/DebugScreen.cs
@@ -38,9 +38,9 @@ namespace Snake.Screens
 
             //base data
             DrawDebugLine(gameInstance.GetType().Assembly.GetName().Name + " V:" + typeof(DebugScreen).Assembly.GetName().Version + " FPS:" + "-" + " TPS:" + "-" + " CurrentScreen:" + (gameInstance.PrimaryScreen != null ? gameInstance.PrimaryScreen.GetType().Name : "NULL"), ref line, drawBuffer);
-            DrawDebugLine("LT:" + gameInstance.LastLoopTime.TotalMilliseconds + " UT:" + gameInstance.LastUpdateTime.TotalMilliseconds + " DT:" + gameInstance.LastDrawTime.TotalMilliseconds + " SDT:" + gameInstance.LastScreenDrawTime.TotalMilliseconds + " FDT:" + gameInstance.LastFinalizeDrawTime.TotalMilliseconds, ref line, drawBuffer);
-            DrawDebugLine("ST:" + gameInstance.LastSleepTime.TotalMilliseconds + " LTS:" + gameInstance.LastTargetSleep + " GT:" + gameTime.ElapsedGameTime.TotalMilliseconds + " TTS:" + gameInstance.TargetTimeStep.TotalMilliseconds, ref line, drawBuffer);
-            DrawDebugLine("DGUT:" + lastDebugUpdate.TotalMilliseconds + " DGDT:" + lastDebugDraw.TotalMilliseconds, ref line, drawBuffer);
+            DrawDebugLine("LT:" + FormatToFourPlace(gameInstance.LastLoopTime.TotalMilliseconds) + " UT:" + FormatToFourPlace(gameInstance.LastUpdateTime.TotalMilliseconds) + " DT:" + FormatToFourPlace(gameInstance.LastDrawTime.TotalMilliseconds) + " SDT:" + FormatToFourPlace(gameInstance.LastScreenDrawTime.TotalMilliseconds) + " FDT:" + FormatToFourPlace(gameInstance.LastFinalizeDrawTime.TotalMilliseconds), ref line, drawBuffer);
+            DrawDebugLine("ST:" + FormatToFourPlace(gameInstance.LastSleepTime.TotalMilliseconds) + " LTS:" + FormatToFourPlace(gameInstance.LastTargetSleep) + " GT:" + FormatToFourPlace(gameTime.ElapsedGameTime.TotalMilliseconds) + " TTS:" + FormatToFourPlace(gameInstance.TargetTimeStep.TotalMilliseconds), ref line, drawBuffer);
+            DrawDebugLine("DGUT:" + FormatToFourPlace(lastDebugUpdate.TotalMilliseconds) + " DGDT:" + FormatToFourPlace(lastDebugDraw.TotalMilliseconds), ref line, drawBuffer);
 
             //screens
 
@@ -52,5 +52,10 @@ namespace Snake.Screens
         /// <param name="line">A by ref int to the vertical(Y) position where to draw the line</param>
         /// <param name="drawBuffer">The <see cref="DrawBuffer"/> used to draw to the screen</param>
         private void DrawDebugLine(string text, ref int line, DrawBuffer drawBuffer) => drawBuffer.Write(0, line++, text, ConsoleColor.Cyan);
+
+        /// <summary>Formats a double value with excetly 4 decimal places</summary>
+        /// <param name="value">The value to format</param>
+        /// <returns>A string version of the double with truncated with 4 decimal places</returns>
+        private string FormatToFourPlace(double value) => Math.Round(value, 4).ToString("0.0000");
     }
 }
